9846e6873b0cdddfc7034a8233ee551383e2f22d
MisooB <michal.brcko@gmail.com>
2018-01-25 18:24:56 +0100

    #17  User, Email  fixtures

--- src/AppBundle/DataFixtures/ORM/AppFixture.php
+++ src/AppBundle/DataFixtures/ORM/AppFixture.php
@@ -10,8 +10,18 @@ namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\Role;
 use AppBundle\Entity\User;
+use AppBundle\Entity\Email;
+use AppBundle\Entity\Workplace;
+use AppBundle\Entity\CourseSoftPrerequisite;
+use AppBundle\Entity\Enrolled;
+use AppBundle\Entity\Place;
+use AppBundle\Entity\CourseInstance;
+use AppBundle\Entity\CourseType;
+
+use Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+
 
 
 class AppFixture extends Fixture
@@ -19,7 +29,7 @@ class AppFixture extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        //roles
+        //create user-roles
         $roles = array("ROLE_USER", "ROLE_SUPERVISOR", "ROLE_MASTER", "ROLE_SUBADMIN", "ROLE_ADMIN");
         $arrayOfRole = array();
         for ($i = 0; $i < 5; $i++) {
@@ -30,17 +40,50 @@ class AppFixture extends Fixture
             $manager->persist($role);
         }
 
-        // create 10 users
-        for ($i = 0; $i < 10; $i++) {
+        // create 20 users
+        for ($i = 1; $i <= 20; $i++) {
+
             $user = new User();
+
             $user->setLogin('user' . $i);
             $pass = 'pass' . $i;
             $user->setPassword(password_hash($pass, PASSWORD_DEFAULT));
-
             $user->setRole($arrayOfRole[random_int(0, 4)]);
 
+
+            $this->addReference('user'.$i, $user);  //set reference for this user
+
             $manager->persist($user);
+
         }
+
+        //create 20 emails
+        for ($i = 1; $i <= 20; $i++) {
+            $email = new Email();
+            $email->setEmail('user' . $i . '@domain.com');
+            $email->setUser($this->getReference('user'.$i));    //get reference of user
+
+            //set email to user
+            ($this->getReference('user'.$i))->setSelectedEmail($email);
+
+            $manager->persist($email);
+        }
+
+//
+//        //create 10 workplaces
+//        for ($i = 1; $i <= 10; $i++) {
+//            $workplace = new Workplace();
+//            $workplace->setName('workplace'.$i);
+//            $workplace->setParent($i);
+//            //TODO ostatne vazby
+//
+//            $manager->persist($workplace);
+//        }
+//
+//        //create 10 places
+//        for ($i = 1; $i <= 10; $i++) {
+//            $place = new Place();
+
 
         $manager->flush();
 
